713b2d0f16a5f94496d48a0f3db4fd5c710f0a9b
Luiz Felipe Correia <lfc291006@gmail.com>
2026-05-21 12:58:11 -0300

    Refactor ListaJogos component to use JogoInterface and integrate jogos data; update generos initialization

--- src/app/componentes/lista-jogos/lista-jogos.ts
+++ src/app/componentes/lista-jogos/lista-jogos.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { GeneroDoJogo } from '../jogo/jogo.inteface';
-import { Jogo } from '../jogo/jogo';
+import { GeneroDoJogo, JogoInterface } from '../jogo/jogo.inteface';
+import { jogos } from '../../mock-jogos';
 
 @Component({
   selector: 'app-lista-jogos',
@@ -10,10 +10,27 @@ import { Jogo } from '../jogo/jogo';
 })
 export class ListaJogos implements OnInit{
   generos: GeneroDoJogo[] = [];
-  jogosPorGenero: Map<string, Jogo[]> = new Map();
+  jogosPorGenero: Map<string, JogoInterface[]> = new Map();
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.jogosPorGenero = new Map();
+
+    jogos.forEach((jogo)=> {
+      const generoId = jogo.genero.id
+      if(!this.jogosPorGenero.has(generoId)){
+        this.jogosPorGenero.set(generoId, [])
+      }
+      this.jogosPorGenero.get(generoId)?.push(jogo)
+    })
+
+    this.generos = [
+      {
+        id: "action-adventure",
+        value: "Action-adventure",
+        jogos: this.jogosPorGenero.get("action-adventure") as unknown as any[] ?? []
+      }
+    ]
     
   }
-  
+
 }
