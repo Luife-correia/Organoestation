1a66d6720b74aefc38fb7e2997e468b080e3b61d
Luiz Felipe Correia <lfc291006@gmail.com>
2026-06-09 22:04:10 -0300

    Update mock-jogos with additional game entries and improve formatting

--- src/app/mock-jogos.ts
+++ src/app/mock-jogos.ts
@@ -1,21 +1,250 @@
 import { Jogo } from "./componentes/jogo/jogo";
 import { JogoInterface } from "./componentes/jogo/jogo.inteface";
 
-export const jogos : JogoInterface[] = [
+export const jogos: JogoInterface[] = [
 
-{
-    nome: "Ben 10: Protector of Earth",
-    desenvolvedor: "High Voltage Software",
-    favorito: false,
-    genero: {
-        id:"action-adventure",
-        value:"Action-adventure",
-        jogos: []
+    {
+        nome: "Ben 10: Protector of Earth",
+        desenvolvedor: "High Voltage Software",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://static.wikia.nocookie.net/ben10/images/1/16/Protector_of_earth.PNG/revision/latest?cb=20120504065112&path-prefix=pt"
+
     },
-    imagem:"https://static.wikia.nocookie.net/ben10/images/1/16/Protector_of_earth.PNG/revision/latest?cb=20120504065112&path-prefix=pt"
+    {
+        nome: "God of War II",
+        desenvolvedor: "Santa Monica Studio",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://i5.walmartimages.com/asr/3ac8e0b5-76d1-4995-9570-d431696221eb_1.7ac304ae3667075f6348a7a065044d54.jpeg"
+    },
+    {
+        nome: "Shadow of the Colossus",
+        desenvolvedor: "Team Ico",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.72e20001a12a26a8463ee3f8b6a9b5f2?rik=PltA6%2bqOM26yQA&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Devil May Cry 3",
+        desenvolvedor: "Capcom",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://i.3djuegos.com/juegos/3716/devil_may_cry_3/fotos/ficha/devil_may_cry_3-1691940.jpg"
+    },
+    {
+        nome: "Prince of Persia: The Sands of Time",
+        desenvolvedor: "Ubisoft",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://tse3.mm.bing.net/th/id/OIP.VBIWgRpkiBv2xmeRnPjj8AHaLu?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        nome: "Tekken 5",
+        desenvolvedor: "Namco",
+        favorito: false,
+        genero: {
+            id: "fighting",
+            value: "Fighting",
+            jogos: []
+        },
+        imagem: "https://tse2.mm.bing.net/th/id/OIP.WgE2rWhIsaYty2De9gNgmQHaKd?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        nome: "Mortal Kombat: Armageddon",
+        desenvolvedor: "Midway Games",
+        favorito: false,
+        genero: {
+            id: "fighting",
+            value: "Fighting",
+            jogos: []
+        },
+        imagem: "https://www.lukiegames.com/assets/images/PS2/ps2_mortal_kombat_armageddon_premium_p_dprtn5.jpg"
+    },
+    {
+        nome: "Dragon Ball Z: Budokai Tenkaichi 3",
+        desenvolvedor: "Spike",
+        favorito: false,
+        genero: {
+            id: "fighting",
+            value: "Fighting",
+            jogos: []
+        },
+        imagem: "https://retromania.gg/statics/assets/covers/playstation-2/dragon-ball-z-budokai-tenkaichi-3-europe-australia-en-ja-fr-de-es-it-playstation-2_1492457094.jpg"
+    },
+    {
+        nome: "Soulcalibur II",
+        desenvolvedor: "Namco",
+        favorito: false,
+        genero: {
+            id: "fighting",
+            value: "Fighting",
+            jogos: []
+        },
+        imagem: "https://tse3.mm.bing.net/th/id/OIP.zoro3vvii33y5_B7626R8AHaKf?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        nome: "Crash Twinsanity",
+        desenvolvedor: "Traveller's Tales",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://bdjogos.com.br/capas/16648-crash-twinsanity-playstation-2-capa-1.jpg"
+    },
+    {
+        nome: "Resident Evil 4",
+        desenvolvedor: "Capcom",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://www.vgdb.com.br/gf/fotos/games/media_49769/resident-evil-4--49769.jpg"
+    },
+    {
+        nome: "Burnout 3: Takedown",
+        desenvolvedor: "Criterion Games",
+        favorito: false,
+        genero: {
+            id: "racing",
+            value: "Racing",
+            jogos: []
+        },
+        imagem: "https://gamefaqs.gamespot.com/a/box/3/0/0/58300_front.jpg"
+    },
+    {
+        nome: "Need for Speed: Underground 2",
+        desenvolvedor: "EA Black Box",
+        favorito: false,
+        genero: {
+            id: "racing",
+            value: "Racing",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.82b6520e423f63d9d3d73bd7f82b2324?rik=7vbFSzFH0a1QsQ&riu=http%3a%2f%2fwww.lukiegames.com%2fassets%2fimages%2fPS2%2fps2_need_for_speed_underground_2-110214.jpg&ehk=mqiZGb52ac2hd1oBk4GHCrjzQcXUa3b2aOjsepx1evg%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Midnight Club 3: DUB Edition",
+        desenvolvedor: "Rockstar San Diego",
+        favorito: false,
+        genero: {
+            id: "racing",
+            value: "Racing",
+            jogos: []
+        },
+        imagem: "https://i5.walmartimages.com/seo/Restored-Midnight-Club-3-Dub-Edition-Remix-PlayStation-2-Refurbished_b1f5146d-a934-45fe-923a-62a35c1a0a22.419af456af756979de77c35ad238f19e.jpeg"
+    },
+    {
+        nome: "Jak X: Combat Racing",
+        desenvolvedor: "Naughty Dog",
+        favorito: false,
+        genero: {
+            id: "racing",
+            value: "Racing",
+            jogos: []
+        },
+        imagem: "https://www.lukiegames.com/assets/images/PS2/ps2_jak_x_combat_racing_p_fjgo0a.jpg"
+    },
+    {
+        nome: "Marvel: Ultimate Alliance",
+        desenvolvedor: "Raven Software",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://retromania.gg/statics/assets/covers/playstation-2/marvel-ultimate-alliance-usa-playstation-2_1477587444.jpg"
+    },
+    {
+        nome: "Final Fantasy X",
+        desenvolvedor: "Square Enix",
+        favorito: false,
+        genero: {
+            id: "rpg",
+            value: "RPG",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.15473bd957f32ec32baa0ae0a4f135b1?rik=DhZRbXr2vk6HBg&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Kingdom Hearts II",
+        desenvolvedor: "Square Enix",
+        favorito: false,
+        genero: {
+            id: "rpg",
+            value: "RPG",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.196c6537ad0492796d0ab67567023aef?rik=icVKUpyVaSM5Zg&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Persona 4",
+        desenvolvedor: "Atlus",
+        favorito: false,
+        genero: {
+            id: "rpg",
+            value: "RPG",
+            jogos: []
+        },
+        imagem: "https://m.media-amazon.com/images/I/91fc4SvMR9L._AC_SL1500_.jpg"
+    },
+    {
+        nome: "Dragon Quest VIII",
+        desenvolvedor: "Level-5",
+        favorito: false,
+        genero: {
+            id: "rpg",
+            value: "RPG",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.55cd33ee16444e5ffde8a5d67ef7b769?rik=%2f%2f6lSwzWWtYgLg&riu=http%3a%2f%2fwww.mobygames.com%2fimages%2fcovers%2fl%2f272849-dragon-quest-viii-journey-of-the-cursed-king-playstation-2-front-cover.jpg&ehk=1anCys574LcjcFbGj7RDsRG4AiTfeF4hW5PqA8cgu0g%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Bomba Patch",
+        desenvolvedor: "Modders Brasileiros",
+        favorito: false,
+        genero: {
+            id: "sports",
+            value: "Sports",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.b0885c6e953090794f4865ee69a742b1?rik=vBKXMUhRF%2bTk1g&riu=http%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0401%2f5846%2f2113%2fproducts%2fPS2-SuperBombaPatch2010.fw_1200x1200.png%3fv%3d1679437714&ehk=5lgzdrgRha%2bLaeCzFZAsVNHl6XEkAMQ%2fC9PgqK4vb5g%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+        nome: "Ghost Rider",
+        desenvolvedor: "Climax Group",
+        favorito: false,
+        genero: {
+            id: "action-adventure",
+            value: "Action-adventure",
+            jogos: []
+        },
+        imagem: "https://th.bing.com/th/id/R.a7f5a75cfdee3a841cac51b9a9c381d3?rik=dOO87c2Y%2fuQRtA&riu=http%3a%2f%2fmedia.ign.com%2fgames%2fimage%2fobject%2f681%2f681942%2fGhostRider-2k_PS2_FoB.jpg&ehk=OOuLuh%2fvASLBzFdBMrkyjYRwnwEWlwf1k3oMh5%2ff%2bjo%3d&risl=&pid=ImgRaw&r=0"
+    }
 
-}
-
-
-    
-]
+];
